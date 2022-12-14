d6b8eeed4e3b0e453edba560d6b57b8e9f3b6250
zixingkong <yayunwang2016@163.com>
2022-12-14 09:57:15 +0800

    feat: 完成第11章第5 编程题

--- ProgrammingExercise/chapter_11/exec_5.c
+++ ProgrammingExercise/chapter_11/exec_5.c
@@ -4,3 +4,54 @@
 给函数提供输入值。
  */
 
+#include <stdio.h>
+#define LIMIT 50
+
+char *findchar(char *str, const char ch);
+
+int main()
+{
+    char string[LIMIT];
+    char *choice;
+    char ch;
+    printf("Enter a string to search: ");
+    fgets(string, LIMIT, stdin);
+    while (string[0] != '\n')
+    {
+        printf("Enter a character to search for: ");
+        ch = getchar();
+        if (ch != '\n')
+        {
+            while (getchar() != '\n')
+            {
+                continue;
+            }
+        }
+        choice = findchar(string, ch);
+        if (choice == NULL)
+        {
+            printf("Character %c not found in %s", ch, string);
+        }
+        else
+        {
+            printf("Character %c found at index %lu in %s", ch, choice - string, string);
+        }
+        printf("Enter a string to search(empty line to quit):");
+        fgets(string, LIMIT, stdin);
+    }
+    puts("Bye");
+    return 0;
+}
+
+char *findchar(char *str, const char ch)
+{
+    while (*str != '\0')
+    {
+        if (*str == ch)
+        {
+            return str;
+        }
+        str++;
+    }
+    return NULL;
+}
